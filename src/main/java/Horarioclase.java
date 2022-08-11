a7faaf8639222b14ab6168c3d3b17af7e049ba0a
lore <lore@DESKTOP-T1K14EA>
2022-08-10 21:25:33 -0400

    inicio de aplicacion

---
+++ src/main/java/Horarioclase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author lore
+ */
+public class Horarioclase {
+    public static void main(String[] args) {
+        System.out.println("Lunes\tMartes\tMiercoles\tjueves\tviernes");
+        System.out.println("=====\t=====\t=====\t=====\t=====");
+        System.out.println("Proyecto\tArquitectura\tProgramacion\tAuditoria\tIng");
+}
+    }
